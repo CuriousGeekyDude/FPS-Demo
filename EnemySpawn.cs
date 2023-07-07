93ea0dd37c5f7362ba1ecd5ff83a675ce5a731cf
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-08 03:35:31 +0430

    Initialize fireball in AITarget

--- EnemySpawn.cs
+++ EnemySpawn.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab = null;
-    [SerializeField] private GameObject fireballPrefab = null;
+    
     private GameObject enemy;
-    private GameObject fireball;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +22,6 @@ public class EnemySpawn : MonoBehaviour
             enemy = Instantiate(enemyPrefab) as GameObject;
             enemy.transform.position = new Vector3(0, 1.5f, 0);
         }
-        if(fireball == null) {
-            Vector3 fireballPosition = enemy.transform.position;
-            fireballPosition.z += 1f;
-            fireball = Instantiate(fireballPrefab) as GameObject;
-            fireball.transform.position = fireballPosition;
-        }
+        
     }
 }
