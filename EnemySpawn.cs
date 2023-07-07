611119bd2001b1efb43530860cbfc190bc794953
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-08 00:30:38 +0430

    Ensure that fireball is constantly checked to exist in the scene
    In order to make sure that the fireball prefab
    is constantly respawned in the scene after each deletion;
    I check every frame to see if the cloned fireball is null
    or not. If it is null, it is cloned frame fireballPrefab and
    positioned in front of the enemyAI that aimlessly moves around.

--- EnemySpawn.cs
+++ EnemySpawn.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab = null;
+    [SerializeField] private GameObject fireballPrefab = null;
     private GameObject enemy;
+    private GameObject fireball;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,12 @@ public class EnemySpawn : MonoBehaviour
         if(enemy == null) {
             enemy = Instantiate(enemyPrefab) as GameObject;
             enemy.transform.position = new Vector3(0, 1.5f, 0);
+        }
+        if(fireball == null) {
+            Vector3 fireballPosition = enemy.transform.position;
+            fireballPosition.z += 1f;
+            fireball = Instantiate(fireballPrefab) as GameObject;
+            fireball.transform.position = fireballPosition;
         }
     }
 }
