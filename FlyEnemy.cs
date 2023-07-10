78d519f9dbfc72c323fd360668811d5359f61940
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-10 07:33:58 +0430

    Check the plane by the z coordinate and not the y
    The game objects created by unity have a local coordinate
    system attached to their centers. When we do a rotation, this
    local system moves as well. In my case, I rotated the object
    by 90 degrees around the local x-axis. This caused the local
    z-axis to switch places with the local y-axis. Therefore, we
    ought to check the z-coordinate of the given point to determine
    which plane we are on.

--- FlyEnemy.cs
+++ FlyEnemy.cs
@@ -7,9 +7,9 @@ public class FlyEnemy : MonoBehaviour
     
     [SerializeField] private float valueOfDisplacementZ = 0.1f;
 
-    private bool IsInInnerCircleFloor(Vector3 point,float planeOfCircle)
+    private bool IsInInnerCircleFloor(Vector3 point, float planeOfCircle)
     {
-        if(point.y != planeOfCircle) {
+        if(point.z != planeOfCircle) {
             return false;
         }
 
@@ -22,11 +22,20 @@ public class FlyEnemy : MonoBehaviour
     }
 
 
-    private void AIMovement()
+/*    private void AIMovement(float planeOfFlight)
     {
-        
-    }
+        var pos = this.transform.TransformPoint(Vector3.forward*2f);
 
+        if(IsInInnerCircleFloor(pos, planeOfFlight)) {
+            this.transform.Translate(new Vector3(0, 0, valueOfDisplacementZ));
+            return;
+        }
+        else {
+            var valueOfRotationY = Random.Range(0, 360);
+            this.transform.Rotate(new Vector3(0, valueOfRotationY, 0));
+        }
+    }
+*/
     void Start()
     {
         
@@ -35,6 +44,6 @@ public class FlyEnemy : MonoBehaviour
     
     void Update()
     {
-        
+        AIMovement(14f);
     }
 }
