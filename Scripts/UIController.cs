fc4a9b2b98d4ca58c16c94eef97416ab068f8607
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-21 03:43:01 +0430

    Show the time passed since the beg. in seconds

--- Scripts/UIController.cs
+++ Scripts/UIController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
+    [SerializeField] private TMP_Text timePastSinceStart;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timePastSinceStart.text = $"Seconds: {Time.realtimeSinceStartup.ToString()}";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timePastSinceStart.text = $"Seconds: {Time.realtimeSinceStartup.ToString()}";
     }
 }
